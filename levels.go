b113688f7137e7be960584f1b0ac1ccf0fc746b1
Matthew Gleich <matthewgleich@gmail.com>
2020-08-11 13:34:31 -0400

    ♻️ Refactor to log error instead of return
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- levels.go
+++ levels.go
@@ -1,41 +1,57 @@
 package logoru
 
-import "github.com/fatih/color"
+import (
+	"log"
+
+	"github.com/fatih/color"
+)
 
 var levels = []string{"DEBUG", "INFO", "SUCCESS", "WARNING", "ERROR", "CRITICAL"}
 
 // Output a debugging message
-func Debug(msg interface{}) error {
+func Debug(msg interface{}) {
 	err := output(levels[0], color.New(color.FgBlue, color.Bold), msg)
-	return err
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // Output an info message
-func Info(msg interface{}) error {
+func Info(msg interface{}) {
 	err := output(levels[1], color.New(color.FgWhite, color.Bold), msg)
-	return err
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // Output a success message
-func Success(msg interface{}) error {
+func Success(msg interface{}) {
 	err := output(levels[2], color.New(color.FgGreen, color.Bold), msg)
-	return err
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // Output a warning message
-func Warning(msg interface{}) error {
+func Warning(msg interface{}) {
 	err := output(levels[3], color.New(color.FgYellow, color.Bold), msg)
-	return err
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // Output an error message
-func Error(msg interface{}) error {
+func Error(msg interface{}) {
 	err := output(levels[4], color.New(color.FgRed, color.Bold), msg)
-	return err
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // Output a critical message
-func Critical(msg interface{}) error {
+func Critical(msg interface{}) {
 	err := output(levels[5], color.New(color.FgWhite, color.BgRed, color.Bold), msg)
-	return err
+	if err != nil {
+		log.Fatal(err)
+	}
 }
